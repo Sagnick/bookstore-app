c10c179e9880d6f80df33418b04dbe861f71c28e
Sagnick Banerjee <sagnickbanerjee4u@gmail.com>
2022-01-17 09:28:56 +0530

    CI/CD pipeline

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+describe('App component', ()=>{
+    it('should render app with error', ()=>{
+        const {asFragment} = render(<App/>);
+        expect(asFragment()).toMatchSnapshot();
+    })
+})
